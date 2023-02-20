01f615ee726c87f3914cec38471bf0190d1be434
Jhamil <jhamil.arroyo.a@gmail.com>
2023-02-19 23:54:59 -0500

    location

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -9,6 +9,7 @@ import {
 } from "../css-styled/location.components";
 import { AsideContact } from "./min/cardAside";
 import { DirectionHotels } from "./min/cardHotels";
+import contact from "/assets/img/contact.png";
 
 import img1 from "../../assets/img/gps.png";
 import img2 from "../../assets/img/chat.png";
@@ -46,7 +47,7 @@ export const Location = () => {
       <LocationWraperTop>
         <LocationInfo>
             <Img
-              src="../../assets/img/contact.png"
+              src={contact}
               alt=""
             />
           <WrapperAside>

89eb5f5cdeb60fc55972d8002fc6f3af2a911317
Jhamil <jhamil.arroyo.a@gmail.com>
2023-02-19 23:40:59 -0500

    counter1

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,6 +1,9 @@
 import CountUp from 'react-countup';
 import '../styles/bgCounter.css'
 import bocina from "/assets/img/bocina.png"
+import hombre from "/assets/img/hombre.png";
+import ticket from "/assets/img/ticket.png"
+import manos from "/assets/img/manos.png";
 
 export const Counter = () => {
   return (
@@ -16,14 +19,14 @@ export const Counter = () => {
               duration={2}
               delay={0}
             />
-            <span className='up'>+</span>
+            <span className="up">+</span>
           </h1>
           <span className="tx-4">speakers</span>
         </div>
       </div>
 
       <div className="container-4">
-        <img className="img-4" src="../../assets/img/hombre.png" alt="des" />
+        <img className="img-4" src={hombre} alt="des" />
         <div>
           <h1 className="mini-con">
             <CountUp
@@ -33,14 +36,14 @@ export const Counter = () => {
               duration={2}
               delay={0}
             />
-            <span className='up'>+</span>
+            <span className="up">+</span>
           </h1>
           <span className="tx-4">metas</span>
         </div>
       </div>
 
       <div className="container-4">
-        <img className="img-4" src="../../assets/img/ticket.png" alt="des" />
+        <img className="img-4" src={ticket} alt="des" />
         <div>
           <h1 className="mini-con">
             <CountUp
@@ -50,14 +53,14 @@ export const Counter = () => {
               duration={2}
               delay={0}
             />
-            <span className='up'>+</span>
+            <span className="up">+</span>
           </h1>
           <span className="tx-4">tickets</span>
         </div>
       </div>
 
       <div className="container-4">
-        <img className="img-4" src="../../assets/img/manos.png" alt="des" />
+        <img className="img-4" src={manos} alt="des" />
         <div>
           <h1 className="mini-con">
             <CountUp
@@ -67,7 +70,7 @@ export const Counter = () => {
               duration={2}
               delay={0}
             />
-            <span className='up'>+</span>
+            <span className="up">+</span>
           </h1>
           <span className="tx-4">strong</span>
         </div>
